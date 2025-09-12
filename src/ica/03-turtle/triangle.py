be19d8ff8353e66e222966d49f1c1e869e05ca5e
doryfulcher <dfulcher@macalester.edu>
2025-09-12 10:15:10 -0500

    mm

--- src/ica/03-turtle/triangle.py
+++ src/ica/03-turtle/triangle.py
@@ -6,7 +6,8 @@ my_turtle.color("blue")
 my_turtle.forward(50)
 my_turtle.left(45)
 my_turtle.forward(50)
-
+my_turtle.left(155)
+my_turtle.forward(100)
 window.exitonclick() # this line closes the window when user click
 
 
