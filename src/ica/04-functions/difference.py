d367f233f79d0982b1d8560ec83f39544a22e917
doryfulcher <dfulcher@macalester.edu>
2025-09-15 10:36:10 -0500

    04

--- src/ica/04-functions/difference.py
+++ src/ica/04-functions/difference.py
@@ -1,8 +1,10 @@
 def smallest_diff(x, y, z):
+    assert type(x) == int and type(y) == int and type(z) == int
     diff1 = abs(x - y)
     diff2 = abs(y - z)
     diff3 = abs(x - z)
     min_diff = min(diff1, diff2, diff3)
+    print(diff1, diff2, diff3, 'return value:', min_diff)
     return min_diff
 
 smallest_diff(3, 9, 5)
---
+++ src/ica/04-functions/my_avg.py
@@ -0,0 +1,7 @@
+def ave3(x,y,z):
+    average=(x+y+z)/3
+    return average
+
+res = ave3(2, 3.0, 4.0)
+print(res)
+
