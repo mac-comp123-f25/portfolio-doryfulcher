407749655378ab3f672ae4f7c2bb9b99f6054db8
doryfulcher <dfulcher@macalester.edu>
2025-09-29 10:20:04 -0500

    ICA

---
+++ src/ica/06-booleans+conditionals/my_tele_turtle.py
@@ -0,0 +1,21 @@
+import turtle
+
+def tele_turtle(n):
+  win = turtle.Screen()
+  tele_t = turtle.Turtle()
+  for i in range(n):
+    move = input("Enter next move: ")
+    do_move(tele_t, move)
+  win.exitonclick()
+
+def do_move(turt, move):
+    if move == 'f':
+        turt.forward(15)
+    elif move == 'b':
+        turt.backward(15)
+    elif move == 'r':
+        turt.right(90)
+    elif move == 'l':
+        turt.left(90)
+    else:
+        print("Invalid move! Use 'f', 'b', 'r', or 'l'.")
