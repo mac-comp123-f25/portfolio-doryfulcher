85083fed322de3d2c168ac1e94c171c0c8621dae
doryfulcher <dfulcher@macalester.edu>
2025-10-09 21:00:39 -0500

    ica

--- src/ica/10-lists/basics.py
+++ src/ica/10-lists/basics.py
@@ -1,5 +1,15 @@
-def every_other(input_list):
-    return input_list[::2]
+def every_other(list1):
+    list2=[]
+    for x in range(0, len(list1),2 ):
+        list2.append(list1[x])
+    return list2
+print(every_other([1,2,3,4,5,6]))
 
-def sum_positive(input_list):
-    return sum(input_list)
+def sum_positive(list1):
+    total =0
+    for x in list1:
+        if x>0:
+            total += x
+    return total
+print(sum_positive([-1,2,-5,4,5,6]))
+
