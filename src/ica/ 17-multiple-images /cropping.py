be926ba6a873796851e7e6a228d885f4fd4e5955
doryfulcher <dfulcher@macalester.edu>
2025-11-17 10:25:09 -0600

    ICA

--- src/ica/ 17-multiple-images /cropping.py
+++ src/ica/ 17-multiple-images /cropping.py
@@ -2,19 +2,20 @@ from src.ica.helpers.imageTools import *
 from src.ica.helpers.dummyWindow import *
 
 def crop_picture(image, x_coord, y_coord,width, height):
+    new_pic=Picture(width, height)
     for target_x in range(width):
         for target_y in range(height):
             source_x = x_coord + target_x
             source_y = y_coord + target_y
-            source_pixel = image.getPixel(image, source_x, source_y)
-            target_pixel = image.getPixel(image, target_x, target_y)
-            image.setColor(target_pixel, image.getColor(source_pixel))
+            color=image.getColor(source_x, source_y)
+            new_pic.setColor(target_x, target_y, color)
 
-    return image
+    return new_pic
 
 dam = Picture("../SampleImages/hooverDam.jpg")
 dam_crop1 = crop_picture(dam, 260, 90, 240, 210)
 dam_crop2 = crop_picture(dam, 100, 150, 100, 150)
+dam.show()
 dam_crop1.show()
 dam_crop2.show()
 
